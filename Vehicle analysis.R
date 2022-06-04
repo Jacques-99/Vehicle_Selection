fa2a3efe6690e77d6513c5fb94ffd077bd1e8114
unknown <jacques09dp@gmail.com>
2022-06-04 16:50:33 +0200

    importing done

--- Vehicle analysis.R
+++ Vehicle analysis.R
@@ -1,6 +1,11 @@
 rm(list = ls())
 library(tidyverse)
 
-cars <- read_csv(file = "./PETROL.csv", col_types = c("cc"))
+cars <- read_csv(file = "./PETROL.csv", col_types = c('cccndndn'), col_names = c("make"
+                  ,"model", "type", "cylinder","engineSize(L)","tankSize(L)"
+                  , "consumption(L/100km)", "range(km)"), skip = 1)
 
-length(unique(cars$MAKE))
+cars <- cars[order(cars$`consumption(L/100km)`, decreasing = FALSE),]
+
+clean <- cars[cars$`consumption(L/100km)` <= 5,c("make",'model','consumption(L/100km)','tankSize(L)')]
+
